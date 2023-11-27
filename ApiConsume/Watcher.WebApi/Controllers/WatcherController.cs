127aadc02527538e7ccc7e07258d1461c5239be0
YesimEren <sertyessim@gmail.com>
2023-11-27 14:42:35 +0300

    Add project files.

---
+++ ApiConsume/Watcher.WebApi/Controllers/WatcherController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Watcher.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WatcherController : ControllerBase
+    {
+        private bool SanalMakina() //IsVirtualMachineRunning
+        {
+            string sanalmakinadi = "yesubuntu"; 
+            string output = VBoxManageCommand($"showvminfo \"{sanalmakinadi}\"");
+
+            return output.Contains("running");
+        }
+
+        private string VBoxManageCommand(string arguments) 
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = "VBoxManage";
+            process.StartInfo.Arguments = arguments;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.CreateNoWindow = true;
+            process.Start();
+
+            string output = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+            return output;
+        }
+
+        private void LogToFile(string logMessage)
+        {
+            string logFile = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Log.txt"); //string logFile = Path.Combine(Directory.GetCurrentDirectory(), "Log.txt");
+            using (StreamWriter writer = System.IO.File.AppendText(logFile))
+            {
+                writer.WriteLine($"{DateTime.Now} - {logMessage}");
+            }
+        }
+
+        [HttpGet("status")]
+        public IActionResult VBoxStatus() 
+        {
+            bool isRunning = SanalMakina();
+            return Ok(new { IsRunning = isRunning });
+        }
+
+        [HttpPost("start")]
+        public IActionResult StartVirtualMachine()
+        {
+            string sanalmakinadi = "yesubuntu";
+            ProcessStartInfo processStartInfo = new ProcessStartInfo
+            {
+                FileName = "VBoxManage",
+                Arguments = $"startvm \"{sanalmakinadi}\"",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true
+            };
+
+            using (Process process = new Process())
+            {
+                process.StartInfo = processStartInfo;
+                process.Start();
+                process.WaitForExit();
+            }
+
+            LogToFile("Virtual Machine started");
+
+            return Ok(new { Message = "Virtual Machine started successfully" });
+        }
+
+        [HttpPost("stop")]
+        public IActionResult StopVirtualMachine()
+        {
+            string sanalmakinadi = "yesubuntu";
+            Process process = new Process();
+            process.StartInfo.FileName = "VBoxManage";
+            process.StartInfo.Arguments = $"controlvm \"{sanalmakinadi}\" poweroff";
+            process.Start();
+            process.WaitForExit();
+
+            LogToFile("Virtual Machine stopped "); // Buraya sanal makinanın adını ekle sonra.(ubuntuyes sanal amkinası duruldu gibi)
+
+            return Ok(new { Message = "Virtual Machine stopped successfully " });
+        }
+
+    }
+}
---
+++ index.js
@@ -0,0 +1,59 @@
+﻿const express = require('express');
+const axios = require('axios');
+const path = require('path');
+const fs = require('fs');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+
+// Express middleware
+app.use(express.json());
+app.use(cors());
+
+let isVirtualMachineRunning = true;
+
+function updateStatus() {
+    axios.get('http://localhost:5145/api/virtualmachine/status')
+        .then(response => {
+            const isVirtualMachineRunning = response.data.isRunning; 
+            updateVMStatus(isVMRunning);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+
+    //Docker, Let's Encrypt ve Memory durumlarına benzer güncellemleri bueraya yapoılacak.
+}
+
+setInterval(() => {
+    updateStatus();
+}, 5000);
+
+app.get('/status', (req, res) => {
+    res.json({ IsRunning: isVirtualMachineRunning });
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Frontend app listening at http://localhost:${port}`);
+});
+
+// Logları dosyaya kaydet
+function logToFile(logMessage) {
+    const logFile = path.join(__dirname, 'log.txt');
+    fs.appendFileSync(logFile, `${new Date()} - ${logMessage}\n`);
+}
+
+// Virtual Machine durumuna göre log kayıtları
+setInterval(() => {
+    updateStatus();
+    if (isVirtualMachineRunning) {
+        logToFile('Virtual Machine is running.');
+    } else {
+        logToFile('Virtual Machine is stopped.');
+    }
+}, 10000); // Her 10 saniyede bir kontrol et
+
